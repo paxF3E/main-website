9adad9a202bb9032f1189f4fb5336a75b067cf13
Suryanshomar7240 <suryu7240@gmail.com>
2021-07-22 13:04:35 +0530

    responsive

--- code/js/nav.js
+++
@@ -1,7 +0,0 @@
-const administration=document.querySelector(".Administration");
-const academics=document.querySelector(".Academics");
-const research=document.querySelector(".Research");
-const student=document.querySelector(".Student");
-const placement=document.querySelector(".Placement");
-const administration=document.querySelector(".Facilities");
-const administration=document.querySelector(".Administration");
